0916fb59582cfe19b00c8503611e517a844d0f5a
Sarfaraj235 <shaikh.sarfaraj.k.gmail.com>
2026-03-05 08:29:54 +0530

    updated

--- src/state/config/ApiConfig.js
+++ src/state/config/ApiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080"
+export const API_BASE_URL = "ecommerce-server-production-c195.up.railway.app"
 
 export const isValidJwt = (token) =>
     typeof token === "string" &&
